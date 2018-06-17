262be153f49dc33573352c144c69e0da519cc09a
hmarsolla <hmarsolla@hotmail.com>
2018-06-17 02:44:08 -0300

    Added Components and States

--- src/playground/build-it-visibility.js
+++ src/playground/build-it-visibility.js
@@ -1,21 +1,52 @@
-let visibility = true;
-const toggleVisibility = () => {
-    visibility = !visibility;
-    renderApp();
+class Visibility extends React.Component {
+    constructor(props){
+        super(props);
+        this.toggleVisibility = this.toggleVisibility.bind(this);
+        this.state = {
+            visibility : false
+        };
+    };
+    toggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility : ! prevState.visibility
+            };
+        });
+    };
+    render () {
+        return (
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.toggleVisibility}>{this.state.visibility ? "Hide Details" : "Show Details"}</button>
+                {this.state.visibility && <p>Hey! Check this crazy stuff that is fun to see!</p>}
+            </div>
+        );
+    };
 };
 
-const renderApp = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility}>{visibility ? "Hide Details" : "Show Details" }</button>
-            {visibility && <p>Hey! Here you can see some stuff that is crazy to see.</p>}
-        </div>
-    );
+ReactDOM.render(<Visibility />, document.getElementById("app"));
+//------------------------------------------------
+//Old code
+//------------------------------------------------
+
+// let visibility = true;
+// const toggleVisibility = () => {
+//     visibility = !visibility;
+//     renderApp();
+// };
+
+// const renderApp = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggleVisibility}>{visibility ? "Hide Details" : "Show Details" }</button>
+//             {visibility && <p>Hey! Here you can see some stuff that is crazy to see.</p>}
+//         </div>
+//     );
     
-    ReactDOM.render(template, appRoot);
-};
+//     ReactDOM.render(template, appRoot);
+// };
 
-const appRoot = document.getElementById("app");
+// const appRoot = document.getElementById("app");
 
-renderApp();
+// renderApp();
