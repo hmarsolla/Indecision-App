ccac65cb44aac3404eba06a759042d7c04ff20e8
hmarsolla <hmarsolla@hotmail.com>
2018-06-17 23:29:02 -0300

    Added Stateless Functional Components and added localStorage for States

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -8,27 +8,32 @@ class Counter extends React.Component {
             count: 0
         };
     };
+
+    componentDidMount(){
+        const count = parseInt(localStorage.getItem("count"), 10);
+        if(!isNaN(count)) {
+            this.setState(() => ({ count }));
+        };
+    };
+
+    componentDidUpdate(prevProps, prevState){
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem("count", this.state.count);
+        };
+    };
+
     handleAddOne(){
-        this.setState((prevState) => {
-            return {
-                count: prevState.count + 1
-            };
-        });
+        this.setState((prevState) => ({ count: prevState.count + 1 }));
     };
+
     handleMinusOne(){
-        this.setState((prevState) => {
-            return {
-                count: prevState.count - 1
-            };
-        });
+        this.setState((prevState) => ({ count: prevState.count - 1 }));
     };
+
     handleReset(){
-        this.setState(() => {
-            return {
-                count: 0
-            };
-        });
+        this.setState(() => ({ count : 0 }));
     };
+
     render () {
         return (
             <div>
